014332420648abb84674970c2697597c044ccafa
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2024-02-10 16:09:45 +0600

    fetch data using tanstack query

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -22,18 +22,9 @@ const Dashboard = () => {
         navigate('/')
     }
 
-    return (
-        // <div className="">
-        //     <div className="flex justify-start gap-6">
-        //         <div className="px-4 bg-gray-300">
-        //             <Sidebar />
-        //         </div>
-        //         <div className="mt-4 w-full mr-24">
-        //             <Outlet/>
-        //         </div>
-        //     </div>
-        // </div>
+    const isAdmin = true;
 
+    return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
@@ -53,15 +44,25 @@ const Dashboard = () => {
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
                 <div className="menu p-4 w-60 bg-base-200 text-base-content flex flex-col justify-between h-screen">
-                    <div className="full">
-                        <img className="w-24 my-4" src={img} alt="" />
-                        <div className="flex flex-col gap-1">
-                            <Link to='/dashboard/all-members'> <button className="flex items-center gap-2 py-2 border text-left  pl-2 border-blue-600 rounded hover:bg-blue-600 hover:text-white w-full"><BsFillPeopleFill className="text-lg" /> <span>মোট সদস্য</span></button></Link>
-                            <Link><div className="w-full">
-                                <button className="flex items-center gap-2 py-2 border text-left  pl-2 border-blue-600 rounded hover:bg-blue-600 hover:text-white w-full"><FaMoneyCheckDollar className="text-lg" /> <span>লোনের হিসাব</span></button></div></Link>
-                            <Link to='/dashboard/update-profile'> <button className="flex items-center gap-2 py-2 border text-left  pl-2 border-blue-600 rounded hover:bg-blue-600 hover:text-white w-full"><RxUpdate className="text-lg" /> <span>প্রফাইল আপডেট</span></button></Link>
-                        </div>
-                    </div>
+                    {
+                        isAdmin ?
+                            <div className="full">
+                                <img className="w-24 my-4" src={img} alt="" />
+                                <div className="flex flex-col gap-1">
+                                    <Link to='/dashboard/all-members'> <button className="flex items-center gap-2 py-2 border text-left  pl-2 border-blue-600 rounded hover:bg-blue-600 hover:text-white w-full"><BsFillPeopleFill className="text-lg" /> <span>মোট সদস্য</span></button></Link>
+                                    <Link><div className="w-full">
+                                        <button className="flex items-center gap-2 py-2 border text-left  pl-2 border-blue-600 rounded hover:bg-blue-600 hover:text-white w-full"><FaMoneyCheckDollar className="text-lg" /> <span>লোনের হিসাব</span></button></div></Link>
+                                    <Link to='/dashboard/update-profile'> <button className="flex items-center gap-2 py-2 border text-left  pl-2 border-blue-600 rounded hover:bg-blue-600 hover:text-white w-full"><RxUpdate className="text-lg" /> <span>প্রফাইল আপডেট</span></button></Link>
+                                </div>
+                            </div>
+                            :
+                            <div className="full">
+                                <img className="w-24 my-4" src={img} alt="" />
+                                <div className="flex flex-col gap-1">
+                                    <Link to='/dashboard/update-profile'> <button className="flex items-center gap-2 py-2 border text-left  pl-2 border-blue-600 rounded hover:bg-blue-600 hover:text-white w-full"><RxUpdate className="text-lg" /> <span>প্রফাইল আপডেট</span></button></Link>
+                                </div>
+                            </div>
+                    }
                     <div className="py-2">
                         <button onClick={handleLogout} className="flex items-start gap-2"><TbLogout2 className="text-lg" /> <span>লগ আউট</span></button>
                     </div>
--- src/Dashboard/TotalMember.jsx
+++ src/Dashboard/TotalMember.jsx
@@ -1,46 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { Link } from "react-router-dom";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
+import Swal from 'sweetalert2'
+
 
 const TotalMember = () => {
+    const axiosPublic = useAxiosPublic()
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/users');
+            return res.data
+        }
+    })
 
-    const [users, setUsers] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:5000/users')
-            .then(res => res.json())
-            .then(data => setUsers(data))
-    }, [])
-    // const handleDelete = (_id, name) => {
+    const handleDelete = (user) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosPublic.delete(`/user/${user._id}`)
+                .then(res => {
+                    if (res.data.deletedCount > 0) {
+                        refetch();
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                        });
+                    }
+                })
 
-    //     Swal.fire({
-    //         title: 'Are you sure?',
-    //         text: "You won't be able to revert this!",
-    //         icon: 'warning',
-    //         showCancelButton: true,
-    //         confirmButtonColor: '#3085d6',
-    //         cancelButtonColor: '#d33',
-    //         confirmButtonText: 'Yes, delete it!'
-    //     }).then((result) => {
-    //         if (result.isConfirmed) {
-    //             fetch(`http://localhost:5000/delete-book/${_id}`, {
-    //                 method: 'DELETE'
-    //             })
-    //                 .then(res => res.json())
-    //                 .then(data => {
-    //                     console.log(data);
-    //                     if (data.deletedCount > 0) {
-    //                         Swal.fire(
-    //                             'Success!',
-    //                             `${name} has been deleted.`,
-    //                             'success'
-    //                         )
-    //                         const remaining = books.filter(items => items._id !== _id);
-    //                         setBooks(remaining)
-    //                     }
-    //                 })
-    //         }
-    //     })
-    // }
+            }
+        });
+    }
     return (
         <div>
             <div>
@@ -53,6 +54,7 @@ const TotalMember = () => {
                                 <th className="text-center">আইডি নং</th>
                                 <th>সদস্যের নাম</th>
                                 <th>সদস্যের ইমেইল</th>
+                                <th className="text-center">রোল</th>
                                 <th className="text-center">ডিলিট</th>
                             </tr>
                         </thead>
@@ -62,10 +64,18 @@ const TotalMember = () => {
                                     <th className="text-center">{index + 1}</th>
                                     <th className="text-center">{user.userId}</th>
                                     <Link to={`/dashboard/member/${user._id}`}><th>{user.name}</th></Link>
-                                    
+
                                     <th>{user.email}</th>
                                     <th className="text-center">
-                                        <button>
+                                        <select id="ddlViewBy" name="type" className="rounded-md text-center">
+                                            <option value="">Select One</option>
+                                            <option value="admin">এডমিন</option>
+                                            <option value="manager">ম্যনেজার</option>
+                                            <option value="member">মেম্বার</option>
+                                        </select>
+                                    </th>
+                                    <th onClick={() => handleDelete(user)} className="text-center">
+                                        <button >
                                             <MdDelete className="text-lg" />
                                         </button>
                                     </th>
